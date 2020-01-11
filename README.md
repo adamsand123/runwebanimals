1f8479b1081b2d85d798dd2fbc9a976492177629
adamsand123 <adamsandstrom1989@gmail.com>
2020-01-11 13:36:54 +0100

    made script better

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # runwebanimals
-Run webgoat and webwolf in docker
+The Script installs docker if not installed
+
+If docker is installed it will start webgoat through docker on localhost port 8000 and webwolf on 9090
--- docker.sh
+++ docker.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 if [ $(command -v docker) ]; then
-	docker run -d -p 8000:8000 -p 9090:9090 -e TZ=Europe/Amsterdam webgoat/goatandwolf
+	if [ $(docker run -d -p 8080:8080 -p 9090:9090 -e TZ=Europe/Amsterdam webgoat/goatandwolf) ]; then
+		echo "Webgoat and webwolf started."
+		echo "WebGoat running on http://localhost:8080/WebGoat"
+		echo "WebWolf running on http://localhost:9090/WebWolf"
+	else
+		echo "WebGoat and WebWolf already running."
+		echo "WebGoat running on http://localhost:8080/WebGoat"
+		echo "WebWolf running on http://localhost:9090/WebWolf"
+	fi
 else
 	# check dist
 	if [ $(uname -a | grep -i ubuntu) ]; then
